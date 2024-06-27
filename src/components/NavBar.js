79a45519c0a12a25ed077d0e9a81c3c157243332
adendaiy <adenabdib@gmail.com>
2024-06-27 13:39:40 +0100

    my react-list-and-key-labs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,21 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // Mapping the links array to create an array of <a> elements
+  const link = links.map(function(link, index) {
+    return (
+      <a key={index} href={`#${link}`}>
+        {link}
+      </a>
+    );
+  });
+
+  return (
+    <nav>
+      {link} {/* Render the array of <a> elements here */}
+    </nav>
+  );
 }
 
 export default NavBar;
+
